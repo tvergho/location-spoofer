85e2a9c251703c3a733f20d114e3f370c466e78d
Tyler Vergho <tvergho@gmail.com>
2021-08-09 13:44:44 -0400

    feat: working electron build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
+import { rootPath } from 'electron-root-path';
 const fixPath = require('fix-path');
 fixPath();
 
@@ -10,6 +11,7 @@ const childProcess = require('child_process');
 // plugin that tells the Electron app where to look for the Webpack-bundled app code (depending on
 // whether you're running in development or production).
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
+const prefix = process.env.NODE_ENV === 'production' ? `${process.resourcesPath}/libimobiledevice/tools` : '../libimobiledevice/tools';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -59,15 +61,21 @@ app.on('activate', () => {
   }
 });
 
+const logHelper = (funcName: string, err: any, stdout: string, stderr: string) => {
+
+  if (err) log.error(`${funcName} error: `, err);
+  log.info(`${funcName} output: `, stdout);
+  if (stderr) log.info(`${funcName} error output: `, stderr);
+
+}
+
 ipcMain.on('coordsChange', (event, latLng) => {
-  childProcess.execFile("idevicesetlocation", [
+  childProcess.execFile(`${prefix}/idevicesetlocation`, [
     "--",
     latLng.lat,
     latLng.lng,
   ], (err: any, stdout: string, stderr: string) => {
-    log.error(err);
-    log.info(stdout);
-    log.info(stderr);
+    logHelper('Set location', err, stdout, stderr);
 
     event.reply('output', stdout);
     console.log(stdout);
@@ -76,22 +84,19 @@ ipcMain.on('coordsChange', (event, latLng) => {
 });
 
 ipcMain.on('reset', (event) => {
-  childProcess.execFile("idevicesetlocation", [
+  childProcess.execFile(`${prefix}/idevicesetlocation`, [
     "reset"
   ], (err: any, stdout: string, stderr: string) => {
-    log.error(err);
-    log.info(stdout);
-    log.info(stderr);
+    logHelper('Reset', err, stdout, stderr);
+
     console.log(stdout);
     console.log(stderr);
   });
 });
 
 ipcMain.on('getDevices', (event) => {
-  childProcess.execFile("idevicename", [], (err: any, stdout: string, stderr: string) => {
-    log.error(err);
-    log.info(stdout);
-    log.info(stderr);
+  childProcess.execFile(`${prefix}/idevicename`, [], (err: any, stdout: string, stderr: string) => {
+    logHelper('Get devices', err, stdout, stderr);
 
     if (stdout.includes('No device found') || stdout.length == 0) event.reply('deviceNames', []);
     else event.reply('deviceNames', stdout.split(','));
@@ -102,10 +107,8 @@ ipcMain.on('getDevices', (event) => {
 });
 
 ipcMain.on('checkImage', (event) => {
-  childProcess.execFile("ideviceimagemounter", ['-l'], (err: any, stdout: string, stderr: string) => {
-    log.error(err);
-    log.info(stdout);
-    log.info(stderr);
+  childProcess.execFile(`${prefix}/ideviceimagemounter`, ['-l'], (err: any, stdout: string, stderr: string) => {
+    logHelper('Check image mounted', err, stdout, stderr);
 
     if (stdout.includes('No device found')) event.reply('imageMounted', false);
     else event.reply('imageMounted', true);
