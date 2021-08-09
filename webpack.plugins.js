58c025634b3235e31f3e9c87cd16f4bc702938e8
Tyler Vergho <tvergho@gmail.com>
2021-08-09 14:31:47 -0400

    fix: linting

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,9 +1,9 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 module.exports = [
   new ForkTsCheckerWebpackPlugin(),
   new webpack.ExternalsPlugin('commonjs', [
-    'electron'
-  ])
+    'electron',
+  ]),
 ];
